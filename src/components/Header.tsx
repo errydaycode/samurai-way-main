114b21e1ef2b2a0b494ec80ce6140e3f1d3b394b
listopadgeorgii <listopadiogl@gmail.com>
2023-01-12 21:37:05 +0300

    'lesson#12'

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+
+   return <header className={'header'}>
+        <img src="https://w7.pngwing.com/pngs/772/942/png-transparent-vimeo-logo-computer-icons-vimeo-miscellaneous-text-trademark-thumbnail.png"
+            alt=""/>
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const Profile = () => {
+
+    return (
+    <div className='content'>
+        <div>
+            <img src="https://vk-oblozhki.ru/photos/big/ssha-gorod-vysotki-doma-los-andzheles-134-3049.jpg"
+                 alt=""/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Profile

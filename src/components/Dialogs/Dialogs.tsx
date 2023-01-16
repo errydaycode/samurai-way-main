8237a30049c674fc54027b7bacac897160e3c279
listopadgeorgii <listopadiogl@gmail.com>
2023-01-16 23:40:30 +0300

    dialogues navlinks

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to={'/1'}>Georgii</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/2'}>Anna</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/3'}>Marina</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/4'}>Elizaveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/5'}>Work</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/6'}>Vlad</NavLink>
+                </div>
+
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How's it going?</div>
+                <div className={s.message}>Yo</div>
+
+            </div>
         </div>
     )
 }

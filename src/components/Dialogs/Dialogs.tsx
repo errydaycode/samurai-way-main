f403425a39200117018f84b5050ce0e47b7a434b
listopadgeorgii <listopadiogl@gmail.com>
2023-01-17 15:42:50 +0300

    DialogItem types

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
-const DialogItem = (props: any) => {
+
+type DialogItemPropsType = {
+    id: number
+    name: string
+}
+
+const DialogItem = (props: DialogItemPropsType) => {
     return (
         <div className={s.dialog + ' ' + s.active}>
             <NavLink to={'/messages/' + props.id}>{props.name}</NavLink>
@@ -10,23 +16,33 @@ const DialogItem = (props: any) => {
     )
 }
 
+type MessagePropsType = {
+    message: string
+}
+
+const Message = (props: MessagePropsType) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <DialogItem name={'Georgy'} id={'1'}/>
-                <DialogItem name={'Anna'} id={'2'}/>
-                <DialogItem name={'Marina'} id={'3'}/>
-                <DialogItem name={'Elizabeth'} id={'4'}/>
-                <DialogItem name={'Work'} id={'5'}/>
-                <DialogItem name={'Vlad'} id={'6'}/>
+                <DialogItem name={'Georgy'} id={1}/>
+                <DialogItem name={'Anna'} id={2}/>
+                <DialogItem name={'Marina'} id={3}/>
+                <DialogItem name={'Elizabeth'} id={4}/>
+                <DialogItem name={'Work'} id={5}/>
+                <DialogItem name={'Vlad'} id={6}/>
             </div>
 
             <div className={s.messages}>
-                <div className={s.message}>Hello</div>
-                <div className={s.message}>How's it going?</div>
-                <div className={s.message}>Yo</div>
+                <Message message={'Hi'}/>
+                <Message message={'How\'s it going'}/>
+                <Message message={'DasWassUp'}/>
+
 
             </div>
         </div>
